54c99c0581cf8cd7f9968bddb96eeaa2ae02a497
dashasuvorova <151539806+dashasuvorova@users.noreply.github.com>
2026-05-20 21:44:21 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from config import TOKEN
+
 import database
+
 from handlers import register_handlers
+from admin import register_admin_handlers
 
 bot = Bot(token=TOKEN)
 
-dp = Dispatcher(bot)
+storage = MemoryStorage()
+
+dp = Dispatcher(
+    bot,
+    storage=storage
+)
 
 register_handlers(dp)
+
+register_admin_handlers(dp)
 
 if __name__ == "__main__":
 
