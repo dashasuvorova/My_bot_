81fdeb84ec2d7ddd1e8c5976a9d3d86e6aff27df
dashasuvorova <151539806+dashasuvorova@users.noreply.github.com>
2026-05-20 23:51:31 +0400

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -4,26 +4,14 @@ main_keyboard = ReplyKeyboardMarkup(
     resize_keyboard=True
 )
 
-btn1 = KeyboardButton("📅 Расписание")
+btn1 = KeyboardButton("🎼 Расписание")
 btn2 = KeyboardButton("👩‍🏫 Преподаватели")
 btn3 = KeyboardButton("🎵 Дисциплины")
 btn4 = KeyboardButton("📢 Объявления")
 btn5 = KeyboardButton("☎️ Контакты")
-
-# ДНИ НЕДЕЛИ
-
-btn6 = KeyboardButton("Понедельник")
-btn7 = KeyboardButton("Вторник")
-btn8 = KeyboardButton("Среда")
-btn9 = KeyboardButton("Четверг")
-btn10 = KeyboardButton("Пятница")
 
 main_keyboard.add(btn1)
 main_keyboard.add(btn2)
 main_keyboard.add(btn3)
 main_keyboard.add(btn4)
 main_keyboard.add(btn5)
-
-main_keyboard.add(btn6, btn7)
-main_keyboard.add(btn8, btn9)
-main_keyboard.add(btn10)
